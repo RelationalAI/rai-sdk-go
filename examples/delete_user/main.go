7d2d8dbd2b937a3307027d3dd31288d5aa71f798
helmi.nour <helmiinour@gmail.com>
2022-04-22 15:54:21 +0100

    reverting ID to id

--- examples/delete_user/main.go
+++ examples/delete_user/main.go
@@ -23,7 +23,7 @@ import (
 )
 
 type Options struct {
-	ID      string `long:"ID" required:"true" description:"user ID"`
+	ID      string `long:"id" required:"true" description:"user ID"`
 	Profile string `long:"profile" default:"default" description:"config profile"`
 }
 
