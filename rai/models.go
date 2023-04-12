6503503ab5a6fa0a4560d593f05cb084c430796f
Wien Leung <wien.leung@relational.ai>
2023-04-12 14:55:30 -0700

    Add support for Snowflake datastreams on views

--- rai/models.go
+++ rai/models.go
@@ -427,9 +427,10 @@ type SnowflakeDataStream struct {
 	CreatedOn   string `json:"createdOn"`
 	State       string `json:"state"`
 	Snowflake   struct {
-		Database string `json:"database"`
-		Schema   string `json:"schema"`
-		Object   string `json:"object"` // fully qualified object name
+		Database   string `json:"database"`
+		Schema     string `json:"schema"`
+		Object     string `json:"object"` // fully qualified object name
+		ObjectType string `json:"objectType"`
 	} `json:"snowflake"`
 	RAI struct {
 		Database string `json:"database"`
@@ -440,7 +441,7 @@ type SnowflakeDataStream struct {
 type createSnowflakeDataStreamRequest struct {
 	Snowflake struct {
 		Object      string               `json:"object"` // fully qualified object name
-		IsView      bool                 `json:"isView"`
+		ObjectType  string               `json:"objectType"`
 		Role        string               `json:"role"`
 		Warehouse   string               `json:"warehouse"`
 		Credentials SnowflakeCredentials `json:"credentials"` // not-persisted
