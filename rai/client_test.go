7a32ba5c242edda3dfa843f46f16818a0a6b4595
Thiago Tonelli Bartolomei <thiago.tonelli@relational.ai>
2024-08-15 09:51:53 -0400

    Temporarily use solverlib

--- rai/client_test.go
+++ rai/client_test.go
@@ -207,18 +207,18 @@ func TestDatabase(t *testing.T) {
 	modelNames, err := client.ListModelNames(test.databaseName, test.engineName)
 	assert.Nil(t, err)
 	assert.True(t, len(modelNames) > 0)
-	assert.True(t, contains(modelNames, "pkg/std/std/common"))
+	assert.True(t, contains(modelNames, "rel/solverlib"))
 
 	models, err := client.ListModels(test.databaseName, test.engineName)
 	assert.Nil(t, err)
 	assert.True(t, len(models) > 0)
-	model := findModel(models, "pkg/std/std/common")
+	model := findModel(models, "rel/solverlib")
 	assert.NotNil(t, model)
 	if model != nil {
 		assert.True(t, len(model.Value) > 0)
 	}
 
-	model, err = client.GetModel(test.databaseName, test.engineName, "pkg/std/std/common")
+	model, err = client.GetModel(test.databaseName, test.engineName, "rel/solverlib")
 	assert.Nil(t, err)
 	assert.NotNil(t, model)
 	if model != nil {
